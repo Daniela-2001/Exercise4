8dbfc51d859636db4728142844a5c31e9af338d5
Daniela <Daniela@DESKTOP-P4BHM2A>
2023-07-17 10:05:58 +0100

    Adicionar arquivos de projeto.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Daniela";
+            double salariofixo = 0, totalvendas = 0, percentualvendas = 0, salariofinal = 0;
+
+            salariofixo = 3000;
+            totalvendas = 2000;
+            percentualvendas = (percentualvendas / 100) * 15;
+            salariofinal = salariofixo + percentualvendas;
+
+            Console.WriteLine("Nome do funcionário: " + nome);
+            Console.WriteLine("Salário fixo: " + salariofixo);
+            Console.WriteLine("Salário final: "+ salariofinal);    
+        }
+    }
+}
